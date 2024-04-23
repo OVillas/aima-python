aa5ac2f2b5311a6dcc41d7803f3c939ab718b0f0
Gabriel Villarinho <104045571+OVillas@users.noreply.github.com>
2024-04-23 16:18:14 -0300

    Initial commit

---
+++ search_recipiente.py
@@ -0,0 +1,125 @@
+import sys
+from collections import deque
+from utils import *
+
+class Problem:
+    def __init__(self, initial, goal=None, graph=None):
+        self.initial = initial
+        self.goal = goal
+        self.graph = graph
+
+    def actions(self, state):
+        return self.graph[state]
+
+    def result(self, state, action):
+        return action
+
+    def goal_test(self, state):
+        if isinstance(self.goal, list):
+            return is_in(state, self.goal)
+        else:
+            return state == self.goal
+
+class Node:
+    def __init__(self, state, parent=None, action=None):
+        self.state = state
+        self.parent = parent
+        self.action = action
+        self.depth = 0
+        if parent:
+            self.depth = parent.depth + 1
+
+    def __repr__(self):
+        return "<Node {}>".format(self.state)
+
+    def __lt__(self, node):
+        return self.state < node.state
+
+    def expand(self, problem):
+        return [self.child_node(problem, action)
+                for action in problem.actions(self.state)]
+
+    def child_node(self, problem, action):
+        next_state = problem.result(self.state, action)
+        next_node = Node(next_state, self, action)
+        return next_node
+
+    def solution(self):
+        return [node.action for node in self.path()[1:]]
+
+    def path(self):
+        node, path_back = self, []
+        while node:
+            path_back.append(node)
+            node = node.parent
+        return list(reversed(path_back))
+
+    def __eq__(self, other):
+        return isinstance(other, Node) and self.state == other.state
+
+    def __hash__(self):
+        return hash(self.state)
+
+
+def breadth_first_graph_search(problem):
+    node = Node(problem.initial)
+    cont = 0
+    if problem.goal_test(node.state):
+        return node, cont
+    frontier = deque([node])
+    explored = set()
+    while frontier:
+        cont += 1
+        node = frontier.popleft()
+        explored.add(node.state)
+        for child in node.expand(problem):
+            if child.state not in explored and child not in frontier:
+                if problem.goal_test(child.state):
+                    return child, cont
+                frontier.append(child)
+    return None, cont
+
+
+def depth_first_graph_search(problem):
+ 
+    cont = 0
+    frontier = [(Node(problem.initial))]  # Stack
+    explored = set()
+    while frontier:
+        cont += 1
+        node = frontier.pop()
+        if problem.goal_test(node.state):
+            return node, cont
+        explored.add(node.state)
+        frontier.extend(child for child in node.expand(problem)
+                        if child.state not in explored and child not in frontier)
+    return None, cont
+
+def depth_limited_search(problem, limit=50):
+    """[Figure 3.17]"""
+
+    def recursive_dls(node, problem, limit):
+        if problem.goal_test(node.state):
+            return node
+        elif limit == 0:
+            return 'cutoff'
+        else:
+            cutoff_occurred = False
+            for child in node.expand(problem):
+                result = recursive_dls(child, problem, limit - 1)
+                if result == 'cutoff':
+                    cutoff_occurred = True
+                elif result is not None:
+                    return result
+            return 'cutoff' if cutoff_occurred else None
+
+    # Body of depth_limited_search:
+    return recursive_dls(Node(problem.initial), problem, limit)
+
+
+def iterative_deepening_search(problem):
+    """[Figure 3.18]"""
+    for depth in range(sys.maxsize):
+        result = depth_limited_search(problem, depth)
+        if result != 'cutoff':
+            return result
